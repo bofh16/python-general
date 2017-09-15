69aa47208a841369d2097a1823d4bc0b609573e7
bofh16 <bofh@bofh.bg>
2017-09-15 18:11:54 +0200

    Add files via upload

--- tic_tack_toe.py
+++ tic_tack_toe.py
@@ -5,22 +5,34 @@ __author__ = 'bofh'
 
 import re
 
+class bcolors:
+    ENDC      = '\033[0m'
+    BOLD      = '\033[1m'
+    UNDERLINE = '\033[4m'
+    RED       = '\033[91m'
+    GREEN     = '\033[92m'
+    WARNING   = '\033[41m'
+    BLUE      = '\033[94m'
+    HEADER    = '\033[95m'
+    CYAN      = '\033[96m'
+
 #------------------------------------------------------------------------------
 
 def print_board(f_board, f_num = 3):
-    print("\n------------------------------------------\n", " 1", " ", "2", " ", "3")
+    print(bcolors.BOLD + bcolors.BLUE + "  1", "  2", "  3" + bcolors.ENDC)
+    print(bcolors.BOLD + " ___" * f_num)
+
     for i in range(f_num):
-        print(" ---" * f_num)
-        print("|", f_board[i][0], "|", f_board[i][1], "|", f_board[i][2], "|", i + 1)
-    print(" ---" * f_num)
+        print(bcolors.BOLD + "|" + bcolors.ENDC, f_board[i][0], bcolors.BOLD + "|" + bcolors.ENDC, f_board[i][1], bcolors.BOLD + "|" + bcolors.ENDC, f_board[i][2], bcolors.BOLD + "|", bcolors.BLUE + str(i + 1) + bcolors.ENDC)
+        print(bcolors.BOLD + "|___" * f_num + "|" + bcolors.ENDC)
 
 #------------------------------------------------------------------------------
 
 def move_input(f_message, f_player1):
     if f_player1:
-        f_player_str = "Player 1 " + f_message
+        f_player_str = "\nPlayer 1 " + f_message
     else:
-        f_player_str = "Player 2 " + f_message
+        f_player_str = "\nPlayer 2 " + f_message
 
     f_move = input(f_player_str)
     f_move_list = f_move.split(",")
@@ -45,19 +57,21 @@ def check_if_played(f_row, f_col):
 
 # -----------------------------------------------------------------------------
 
-def check_if_filled():
-    for i in range(3):
+def check_if_filled(f_num = 3):
+    for i in range(f_num):
         for j in range(3):
-            if board[i][j] == " ":
+            if (board[i][j] == " "):
                 f_filled = False
                 break
             else:
                 f_filled = True
+        if (f_filled == False):
+            break
 
     if (not f_filled):
         return False
     else:
-        print("\nNo more available moves!\n")
+        print(bcolors.BOLD + bcolors.WARNING + "\nNo more available moves!\n" + bcolors.ENDC)
         return True
 
 # -----------------------------------------------------------------------------
@@ -76,10 +90,10 @@ def check_row_win(f_player1):
                 f_won = False
 
     if (f_won) and (f_player1):
-        print("\nPlayer 1 won!\n")
+        print(bcolors.BOLD + bcolors.RED + "\nPlayer 1 won!\n")
         return True
     elif (f_won) and (not f_player1):
-        print("\nPlayer 2 won!\n")
+        print(bcolors.BOLD + bcolors.CYAN + "\nPlayer 2 won!\n")
         return True
     else:
         return False
@@ -100,10 +114,10 @@ def check_col_win(f_player1):
                 f_won = False
 
     if (f_won) and (f_player1):
-        print("\nPlayer 1 won!\n")
+        print(bcolors.BOLD + bcolors.RED + "\nPlayer 1 won!\n")
         return True
     elif (f_won) and (not f_player1):
-        print("\nPlayer 2 won!\n")
+        print(bcolors.BOLD + bcolors.CYAN + "\nPlayer 2 won!\n")
         return True
     else:
         return False
@@ -131,10 +145,10 @@ def check_diag_win(f_player1):
                 f_won = False
 
     if (f_won) and (f_player1):
-        print("\nPlayer 1 won!\n")
+        print(bcolors.BOLD + bcolors.RED + "\nPlayer 1 won!\n")
         return True
     elif (f_won) and (not f_player1):
-        print("\nPlayer 2 won!\n")
+        print(bcolors.BOLD + bcolors.CYAN + "\nPlayer 2 won!\n")
         return True
     else:
         return False
@@ -147,6 +161,7 @@ if __name__ == "__main__":
     filled = False
     input_message = "- enter your move as row,col: "
     
+    print("\033c")
     print_board(board)
     while (not (won or filled)):
         player1 = True
@@ -155,7 +170,9 @@ if __name__ == "__main__":
         while played:
             row, col = move_input(input_message, player1)
             played = check_if_played(row, col)
-        board[row][col] = "X"
+        
+        print("\033c")
+        board[row][col] = bcolors.RED + "X" + bcolors.ENDC
         print_board(board)
 
         won = check_row_win(player1) or check_col_win(player1) or check_diag_win(player1)
@@ -169,7 +186,8 @@ if __name__ == "__main__":
             while played:
                 row, col = move_input(input_message, player1)
                 played = check_if_played(row, col)
-            board[row][col] = "O"
+            board[row][col] = bcolors.CYAN + "O" + bcolors.ENDC
+            print("\033c")
             print_board(board)
 
             won = check_row_win(player1) or check_col_win(player1) or check_diag_win(player1)
